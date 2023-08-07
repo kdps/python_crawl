dc19fc128744380397144ef2ffa51b556bc92e54
kdp <kdp7584@gmail.com>
2023-08-07 21:56:33 +0900

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup as bs
 from requests.adapters import HTTPAdapter, Retry
 from urllib.parse import urlparse
 from requests.exceptions import ProxyError, SSLError, ConnectTimeout
+from urllib.parse import parse_qs
 
 def getRequest(method = "get", url = "", parameters = {}, proxies = {}, headers = {}):
    try:
@@ -13,12 +14,21 @@ def getRequest(method = "get", url = "", parameters = {}, proxies = {}, headers 
          parsedUri = urlparse(url)
          session.mount(parsedUri.scheme + '://', HTTPAdapter(max_retries=retries))
          
-         encodedParameter = urllib.parse.urlencode(parameters)
-         response = getattr(session, method)(url, proxies=proxies, headers=headers)
+         concatParameters = parse_qs(parsedUri.query)
+         concatParameters.update(parameters)
+         
+         encodedParameter = urllib.parse.urlencode(concatParameters)
+         requestUrl = url.split('?')[0].split('#')[0]
+         
+         if (encodedParameter != ''):
+             requestUrl = requestUrl + '?' + encodedParameter
+         # end if
+         
+         response = getattr(session, method)(requestUrl, proxies=proxies, headers=headers)
          return response
    except (ProxyError, SSLError, ConnectTimeout) as e:
       print(e)
 # end def
 
-response = getRequest("get", "https://www.google.com")
+response = getRequest("get", "https://section.cafe.naver.com/ca-fe/home/search/combinations?q=%EA%B0%80", parameters = {'test': 'test'})
 response = bs(response.text, "html.parser")

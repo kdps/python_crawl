b28bf4e3ba2d94cf3670fea839c1f1ee355842f2
kdp <kdp7584@gmail.com>
2023-08-07 21:19:11 +0900

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -5,10 +5,7 @@ from bs4 import BeautifulSoup as bs
 
 def getRequest(method = "get", url = "", parameters = {}, proxies = {}):
    encodedParameter = urllib.parse.urlencode(parameters)
-
    response = getattr(requests, method)(url, proxies=proxies)
-
-   #response = requests.get(url, proxies=proxies)
    return response
 # end def
 

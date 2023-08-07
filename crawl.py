a214aeff1d65ae91d5edbc9a46f3d15e6b32184e
kdp <kdp7584@gmail.com>
2023-08-07 21:18:56 +0900

    Create crawl.py

---
+++ crawl.py
@@ -0,0 +1,17 @@
+import urllib
+import requests
+import concurrent
+from bs4 import BeautifulSoup as bs
+
+def getRequest(method = "get", url = "", parameters = {}, proxies = {}):
+   encodedParameter = urllib.parse.urlencode(parameters)
+
+   response = getattr(requests, method)(url, proxies=proxies)
+
+   #response = requests.get(url, proxies=proxies)
+   return response
+# end def
+
+response = getRequest("get", "https://www.google.com")
+response = bs(response.text, "html.parser")
+
